15d7ce5aa5a66a82cbd2a874ad398ab8671cdaa0
Joy Dey <111901608+ItsJoy2@users.noreply.github.com>
2025-01-16 17:10:41 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -233,22 +233,22 @@ setInterval(fetchMindPrice, 5000);
 	}
 
 	
-	function openWallet(evt, walletName) {
-        var i, tabcontent, tablinks;
-        tabcontent = document.getElementsByClassName("tabcontent");
-        for (i = 0; i < tabcontent.length; i++) {
-            tabcontent[i].style.display = "none";
-        }
-        tablinks = document.getElementsByClassName("tablinks");
-        for (i = 0; i < tablinks.length; i++) {
-            tablinks[i].className = tablinks[i].className.replace(" active", "");
-        }
-        document.getElementById(walletName).style.display = "block";
-        evt.currentTarget.className += " active";
-    }
-    document.addEventListener("DOMContentLoaded", function() {
-        document.querySelector(".tablinks").click();
-    });
+	// function openWallet(evt, walletName) {
+ //        var i, tabcontent, tablinks;
+ //        tabcontent = document.getElementsByClassName("tabcontent");
+ //        for (i = 0; i < tabcontent.length; i++) {
+ //            tabcontent[i].style.display = "none";
+ //        }
+ //        tablinks = document.getElementsByClassName("tablinks");
+ //        for (i = 0; i < tablinks.length; i++) {
+ //            tablinks[i].className = tablinks[i].className.replace(" active", "");
+ //        }
+ //        document.getElementById(walletName).style.display = "block";
+ //        evt.currentTarget.className += " active";
+ //    }
+ //    document.addEventListener("DOMContentLoaded", function() {
+ //        document.querySelector(".tablinks").click();
+ //    });
 
 
 	// for show kids age
